7127a55b3fbc0b8abce9f9ac18ee3b631ec2f8b3
Beraki Befekadu <drogba20020@gmail.com>
2018-07-20 18:45:53 -0500

    Initial commit: SparkJava behaving

---
+++ src/main/java/hello/Interface/PostInterface.java
@@ -0,0 +1,5 @@
+package hello.Interface;
+
+public interface PostInterface {
+    void addPost(Integer postUserId, Integer postCategory, String postTitle, String postText);
+}
---
+++ src/main/java/hello/Model/Post.java
@@ -0,0 +1,76 @@
+package hello.Model;
+
+public class Post {
+    Integer postId;
+    String postTitle;
+    String postText;
+    Integer postCategory;
+    Integer postUserId;
+    Integer dateCreated;
+    Integer lastModified;
+    Integer status;
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public String getPostText() {
+        return postText;
+    }
+
+    public Integer getPostCategory() {
+        return postCategory;
+    }
+
+    public Integer getPostUserId() {
+        return postUserId;
+    }
+
+    public Integer getDateCreated() {
+        return dateCreated;
+    }
+
+    public Integer getLastModified() {
+        return lastModified;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public void setPostText(String postText) {
+        this.postText = postText;
+    }
+
+    public void setPostCategory(Integer postCategory) {
+        this.postCategory = postCategory;
+    }
+
+    public void setPostUserId(Integer postUserId) {
+        this.postUserId = postUserId;
+    }
+
+    public void setDateCreated(Integer dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public void setLastModified(Integer lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
